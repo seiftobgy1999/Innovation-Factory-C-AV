3afc87345c8797dc5bd8416e9ec06ddd7614124b
Seif El Tobgy <seifeltobgy@Seifs-MacBook-Air.local>
2020-01-26 13:03:52 -0500

    Snow protocol

--- Snow_protocol.py
+++ Snow_protocol.py
@@ -1,7 +1,17 @@
+## @file Snow_protocol.py
+#  @author Mostafa Mohsen, Chris Vishnu, Seif El Tobgy, Saif Fadhel
+#  @brief Provides the Abstract Data Type for Node
+#  @date 26/01/2020
 from Car import *
 from Node import *
 threshhold = 4.5 #m/s^2
 
+## @brief Node constructor
+#  @param car the car is a object that contains that speed and ID of the vehicle
+#  @param node the node is a unit that contains various sensors that measure environmental conditions
+#  @param patch_length the length of the patch of snow
+#  @param distance the distance from the car to the patch of snow
+#  @return a string that represents the set of protocols to follow when you approach a patch of snow
 def Protocol(car, node, patch_length, distance):
     node.dynamic_speed(car)
     vi = car.get_speed()
@@ -15,12 +25,12 @@ def Protocol(car, node, patch_length, distance):
 
     a = abs((vf**2 - vi**2)/(2 * distance))
     a = min(a, threshhold)
-    new = (vi**2 + 2 * a * distance)**0.5
+    new = (vi**2 + 2 * -a * distance)**0.5
     modifier = new / vi 
     car.update_speed(modifier)
 
-    return (car.get_speed())
+    return ("initial V = " + str(vi) +  "; accelerate for a = " + str(-a) + "; untill final V = " + str(new)) 
 
 car1 = Car(10, 80)
-node1 = Node(0, "", -1, 1000000, 0, "light snow")
-Protocol(car1, node, 0, 50, "light snow")
+node1 = Node(0.5, "he", -1, 1000000, 0, "light snow")
+print(Protocol(car1, node1, 30, 50,))
