f034dd15a03590281afcd915c5a3a2614abc6859
Christopher Vishnu <vishnuc@mcmaster.ca>
2020-01-26 11:13:46 -0500

    Modifications to the data_typeMain

--- data_typeMain.py
+++ data_typeMain.py
@@ -1,20 +1,20 @@
 from Car import *
 from Node import *
- 
+
 def main():
     car1 = Car(101, 80)
     video = ""
     audio = ""
     while(True):
-       temp = float(input("What is the current temperature? "))
-       hum = float(input("What is the current humidity? "))
-       pressure = float(input("What is the current atomospheric pressure? "))
-       vibration = float(input("What is the vibration? "))
+       temp = float(input("What is the current temperature in kelvin? "))
+       hum = float(input("What is the current humidity in decimal? "))
+       pressure = float(input("What is the current atomospheric pressure in pascals? "))
+       vibration = float(input("What is the vibration in meters/s? "))
        if(pressure <= 101325):
            if(temp <= 0):
-               video = input("What does the current situation look like? ")
+               video = input("What is the snow level? ")
            else:
-               audio = input("What does it sound like right now? ")
+               audio = input("Is it raining very hard? ")
 
        node1 = Node(hum, audio, temp, pressure, vibration, video)
        print("Current Speed", car1.get_speed())
